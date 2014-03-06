3bdbf65d2294cc591060fbdddf2e909e41979c0f
Sergei Dolgov <dolgovs@gmail.com>
2014-03-06 16:01:53 +0100

    First naive version of paragraph merging
    Does not quite work well, disabled for the moment (MAX_MERGE = 1).
    
    Also split align function into smaller ones for convenience.

--- align.py
+++ align.py
@@ -6,6 +6,7 @@ from os.path import join
 
 MISMATCH_PENALTY = 10
 GAP_PENALTY = 100
+MAX_MERGE = 1
 
 def penalty(e1, e2):
     if e1 == e2:
@@ -15,36 +16,44 @@ def penalty(e1, e2):
 def length_penalty(e1, e2):
     return 100 * abs(len(e1) - len(e2))/(len(e1) + len(e2))
 
-def align(s1, s2, penalty=length_penalty):
+def candidates(s1, s2, i, j, r, penalty):
+    p = penalty(s1[i], s2[j])
+    yield (-1, -1), r[i-1, j-1] + p
+    yield (-1, 0), r[i-1, j] + GAP_PENALTY
+    yield (0, -1), r[i, j-1] + GAP_PENALTY
+    for k in range(2, MAX_MERGE + 1):
+        if i-k >= 0:
+            pm = penalty("".join(s1[i-k+1:i+1]), s2[j])
+            if pm < p:
+                yield (-k, -1), r[i-k, j-1] + 10 + pm
+        if j-k >= 0:
+            pm = penalty(s1[i], "".join(s2[j-k+1:j+1]))
+            if pm < p:
+                yield (-1, -k), r[i-1, j-k] + 10 + pm
+
+def fill_matrix(s1, s2, penalty):
     l1, l2 = map(len, (s1, s2))
     r = numpy.zeros((l1, l2))
-    def candidates(i, j):
-        return (r[i-1, j-1] + penalty(s1[i], s2[j]),
-                r[i-1, j] + GAP_PENALTY,
-                r[i, j-1] + GAP_PENALTY)
     for i in range(l1):
         r[i, 0] = GAP_PENALTY * i
     for i in range(l2):
         r[0, i] = GAP_PENALTY * i
     for i in range(1, l1):
         for j in range(1, l2):
-            r[i, j] = min(candidates(i, j))
+            r[i, j] = min(candidates(s1, s2, i, j, r, penalty), key=itemgetter(1))[1]
     print r
-    def min_index(seq):
-        return min(enumerate(seq), key=itemgetter(1))[0]
-    i, j = l1-1, l2-1
+    return r
+
+def reconstruct_solution(s1, s2, r, penalty):
+    i, j = map(lambda s: len(s) - 1, (s1, s2))
+    print "++", i, j
     result = []
     while i >= 0 and j >= 0:
-        m = min_index(candidates(i, j))
-        if m == 0:
-            result.append((s1[i], s2[j]))
-            i, j = i-1, j-1
-        elif m == 1:
-            result.append((s1[i], None))
-            i -= 1
-        elif m == 2:
-            result.append((None, s2[j]))
-            j -= 1
+        di, dj = min(candidates(s1, s2, i, j, r, penalty), key=itemgetter(1))[0]
+        def take(s, k, dk):
+            return "".join(s[k+dk+1:k+1])
+        result.append((take(s1, i, di), take(s2, j, dj)))
+        i, j = i+di, j+dj
     if i < 0:
         i = 0
     if j < 0:
@@ -58,6 +67,10 @@ def align(s1, s2, penalty=length_penalty):
             result.append((None, s2[j]))
             j -= 1
     return list(reversed(result))
+
+def align(s1, s2, penalty=length_penalty):
+    r = fill_matrix(s1, s2, penalty)
+    return reconstruct_solution(s1, s2, r, penalty)
 
 def format(aligned):
     for p in aligned:
